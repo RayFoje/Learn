7539276c92a31d902e5c861d930bb30ec9516fe6
RayFoj <toni.karp2018@gmail.com>
2024-10-12 18:01:51 +0300

    translate and rename

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,65 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Оберіть функцію, 1(+), 2(-), 3(*), 4(/): ");
-        int sw = scanner.nextInt();
-        switch (sw) {
-            case 1:
-                System.out.println("Було обрано функцію +");
-                SUM();
-                break;
-            case 2:
-                System.out.println("Було обрано функцію -");
-                RIZ();
-                break;
-            case 3:
-                System.out.println("Було обрано функцію *");
-                MNO();
-                break;
-            case 4:
-                System.out.println("Було обрано функцію /");
-                DIL();
-                break;
-            default:
-                System.out.println("Error!!!");
-                break;
-        }
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Select a function, 1(+), 2(-), 3(*), 4(/): ");
+    int sw = scanner.nextInt();
+    switch (sw) {
+      case 1:
+        System.out.println("A function has been selected +");
+        sum();
+        break;
+      case 2:
+        System.out.println("A function has been selected -");
+        dif();
+        break;
+      case 3:
+        System.out.println("A function has been selected *");
+        pro();
+        break;
+      case 4:
+        System.out.println("A function has been selected /");
+        fra();
+        break;
+      default:
+        System.out.println("Error!!!");
+        break;
     }
-    public  static void SUM(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введіть перше число");
-        float a = scanner.nextInt();
-        System.out.println("Введіть друге число");
-        float b = scanner.nextInt();
-        float c = a+b;
-        System.out.println("Сума = " + c);
-    }
-    public  static void RIZ(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введіть перше число");
-        float a = scanner.nextInt();
-        System.out.println("Введіть друге число");
-        float b = scanner.nextInt();
-        float c = a-b;
-        System.out.println("Різниця = " + c);
-    }
-    public  static void MNO(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введіть перше число");
-        float a = scanner.nextInt();
-        System.out.println("Введіть друге число");
-        float b = scanner.nextInt();
-        float c = a*b;
-        System.out.println("Добуток = " + c);
-    }
-    public  static void DIL(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введіть перше число");
-        float a = scanner.nextInt();
-        System.out.println("Введіть друге число");
-        float b = scanner.nextInt();
-        float c = a/b;
-        System.out.println("Частка = " + c);
-    }
+  }
+  public  static void sum(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the first number");
+    float a = scanner.nextInt();
+    System.out.println("Enter the second number");
+    float b = scanner.nextInt();
+    float c = a+b;
+    System.out.println("Sum = " + c);
+  }
+  public  static void dif(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the first number");
+    float a = scanner.nextInt();
+    System.out.println("Enter the second number");
+    float b = scanner.nextInt();
+    float c = a-b;
+    System.out.println("Difference = " + c);
+  }
+  public  static void pro(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the first number");
+    float a = scanner.nextInt();
+    System.out.println("Enter the second number");
+    float b = scanner.nextInt();
+    float c = a*b;
+    System.out.println("Product = " + c);
+  }
+  public  static void fra(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the first number");
+    float a = scanner.nextInt();
+    System.out.println("Enter the second number");
+    float b = scanner.nextInt();
+    float c = a/b;
+    System.out.println("Fraction = " + c);
+  }
 }
